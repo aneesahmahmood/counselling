bae6eaf306300449368c5c8d2a4672a47a04972f
Aneesah Mahmood <aneesah.mahmood1@gmail.com>
2024-06-18 17:43:19 +0100

    added fonts

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,12 +1,23 @@
 import Image from "next/image";
 import React from "react";
 import Gaz from "../photos/Gaz.jpeg";
+import { Cormorant_Garamond, Lustria, Fraunces } from "next/font/google";
+
+
+
+const fraunces = Fraunces({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+
 
 function Intro() {
+  const introText = `${fraunces.className} intro-text`;
   return (
     <>
       <div className="intro-div">
-        <p className="intro-text">
+        <p className={introText}>
           Your journey to mental wellness begins here.
         </p>
         <Image className="image" src={Gaz} alt="Ghazala Aziz" />
--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,25 +1,49 @@
 import { Button } from "antd";
 import Link from "next/link";
 import "../navbar/Navbar.css";
+import { Cormorant_Garamond, Lustria, Fraunces } from "next/font/google";
+
+const cormorant = Cormorant_Garamond({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+const fraunces = Fraunces({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+const lustria = Lustria({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 const Navbar = () => {
+  const titleOneClassName = `${fraunces.className} header-text1`;
+  const titleTwoClassName = `${fraunces.className} header-text2`;
+  const navLinks = `${fraunces.className} navbar`;
   return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/services">Services</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="main-header">
+      <header className="header">
+        <span className={titleOneClassName}>Ghazala Aziz&nbsp;</span>
+        <span className={titleTwoClassName}>Counselling</span>
+      </header>
+      <nav className={navLinks}>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/services">Services</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
