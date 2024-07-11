f9fdecc6d56c54dc6bb998e57e302fcce85de0a3
Aneesah Mahmood <aneesah.mahmood1@gmail.com>
2024-07-11 22:31:56 +0100

    added questions.json

--- components/servicesPage/servicesMain/ServicesMain.jsx
+++ components/servicesPage/servicesMain/ServicesMain.jsx
@@ -25,37 +25,39 @@ function ServicesMain() {
         that you feel supported in our work together.
       </p>
       <h2 className="header-service">WHAT TO EXPECT</h2>
-      <div className="number-div">
-        <p className="numbers">01</p>
-        <p className="text">
-          We&#39;ll start with a brief phone conversation so that I can get a
-          sense of the kind of support that you are looking for. I&#39;ll ask
-          for some basic information to get to know you and also use this time
-          to answer any questions that you might have. If it feels like we are a
-          good fit for working together, we will schedule either an in-person or
-          online appointment.
-        </p>
-      </div>
-      <div className="number-div">
-        <p className="numbers">02</p>
-        <p className="text">
-          In the first few sessions, I will learn more about you and together,
-          we will explore the goals you have for therapy. Do you want to build
-          healthier relationships? Learn how to better manage emotions and
-          stress? Or even identifying patterns that keep us feeling
-          &#34;stuck&#34; in our lives.
-        </p>
-      </div>
-      <div className="number-div">
-        <p className="numbers">03</p>
-        <p className="text">
-          The length and number of sessions will differ depending on your goals
-          and needs for therapy. For some, a handful of sessions to address an
-          acute issue will be enough, versus, others may need consistent, weekly
-          sessions for months or years depending on their needs. We can discuss
-          this in further detail to ensure you are receiving the support that
-          you are looking for. ​
-        </p>
+      <div className="text-div">
+        <div className="number-div">
+          <p className="numbers">01</p>
+          <p className="text">
+            We&#39;ll start with a brief phone conversation so that I can get a
+            sense of the kind of support that you are looking for. I&#39;ll ask
+            for some basic information to get to know you and also use this time
+            to answer any questions that you might have. If it feels like we are
+            a good fit for working together, we will schedule either an
+            in-person or online appointment.
+          </p>
+        </div>
+        <div className="number-div">
+          <p className="numbers">02</p>
+          <p className="text">
+            In the first few sessions, I will learn more about you and together,
+            we will explore the goals you have for therapy. Do you want to build
+            healthier relationships? Learn how to better manage emotions and
+            stress? Or even identifying patterns that keep us feeling
+            &#34;stuck&#34; in our lives.
+          </p>
+        </div>
+        <div className="number-div">
+          <p className="numbers">03</p>
+          <p className="text">
+            The length and number of sessions will differ depending on your
+            goals and needs for therapy. For some, a handful of sessions to
+            address an acute issue will be enough, versus, others may need
+            consistent, weekly sessions for months or years depending on their
+            needs. We can discuss this in further detail to ensure you are
+            receiving the support that you are looking for. ​
+          </p>
+        </div>
       </div>
 
       <div className="table-div">
